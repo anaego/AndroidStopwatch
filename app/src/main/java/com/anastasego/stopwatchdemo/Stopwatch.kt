18171acd3da27ed441e272cec5f84c8766635aa0
Anastasia Egorova <anastasego@ip-172-20-48-134.ec2.internal>
2019-03-19 17:18:22 +0300

    Merged previously written kotlin stopwatch into the android project

---
+++ app/src/main/java/com/anastasego/stopwatchdemo/Stopwatch.kt
@@ -0,0 +1,42 @@
+package com.anastasego.stopwatchdemo
+
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
+
+interface Stopwatch {
+
+    val value: Observable<Long>
+    fun pause()
+    fun resume()
+
+    class Impl(private val scheduler: Scheduler) : Stopwatch {
+
+        override val value = BehaviorSubject.create<Long>().toSerialized()
+        private val consumersCountChange = PublishSubject.create<Long>().toSerialized()
+        private val actualValueCounter = AtomicLong()
+
+        init {
+            consumersCountChange
+                .scan { consumerCount, countChange -> consumerCount + countChange }
+                .filter { consumerCount -> consumerCount in 0..1 }
+                .switchMap { o ->
+                    if (o < 1) {
+                        Observable.never<Long>()
+                    } else {
+                        Observable
+                            .interval(1, TimeUnit.SECONDS, scheduler)
+                            .map { _ -> actualValueCounter.getAndIncrement() }
+                    }
+                }
+                .subscribe(value)
+        }
+
+        override fun pause() = consumersCountChange.onNext(-1)
+
+        override fun resume() = consumersCountChange.onNext(+1)
+    }
+}
